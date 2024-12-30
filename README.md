6e81a73871acc18eeac15587a1ab1121c26c629d
Jeremias Cuello <cuellojeremiasnatanael@gmail.com>
2024-12-30 09:54:29 -0300

    add | jest

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # jest-intro
+
 Introducción al framework de testing Jest
+
+### Requisitos
+
+- [NodeJS](https://nodejs.org/en/download/current)
+- [Jest (Framework de testing)](#instalar-jest)
+
+### Instalar Jest
+```bash
+npm i --save-dev jest
+```
+
+### Instalación
+
+```bash
+npm test
+```
--- isVowel.spec.js
+++ isVowel.spec.js
@@ -1,30 +1,9 @@
-const assert = require("assert");
 const isVowel = require(".");
 
-let failes = 0;
-
-const tests = [];
-const test = (name, fn) => { tests.push({ name, fn }) };
-
 test("isVowel retorna true para a letra 'a'", () => {
-  assert.equal(isVowel("a"), true);
+  expect(isVowel("a")).toBe(true);
 });
 
 test("isVowel retorna true para a letra 'c'", () => {
-  assert.equal(isVowel("c"), false);
+  expect(isVowel("c")).toBe(false);
 });
-
-tests.forEach(({ name, fn }) => {
-  try {
-    fn();
-    console.log(`${name} passed`);
-  } catch (error) {
-    console.error(`${name} failed`);
-    console.error(`Expected ${error.expected} but received ${error.actual}`);
-    failes++;
-  }
-});
-
-if(failes >= 0){
-  console.log("Cantidad de failes: ", failes);
-}
