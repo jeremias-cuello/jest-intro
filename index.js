f360e2875b20f9ea3054e07ef374d4add5787738
Jeremias Cuello <cuellojeremiasnatanael@gmail.com>
2024-12-30 12:01:10 -0300

    dev, add | ignorando str con mayusculas - cantidad de cada vocal

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = char => 'aeiou'.includes(char.toLowerCase());
+const isVowel = char => 'aeiou'.includes(char);
+const countVowels = (str) => [...str].reduce(
+  (acc, char) => {
+    if(isVowel(char)){
+      acc[char]++;
+    }
+
+    return acc;
+  },
+  { a: 0, e: 0, i: 0, o: 0, u: 0 }
+);
+
+module.exports = { isVowel, countVowels };
--- isVowel.spec.js
+++ isVowel.spec.js
@@ -1,15 +1,18 @@
-const isVowel = require(".");
+const { isVowel, countVowels } = require(".");
 
 describe("isVowel", () => {
   it("Deberia retornar true para input 'a'", () => {
-    expect(isVowel("a")).toBe(true);
-  });
-
-  it("Deberia retornar true para input 'A'", () => {
-    expect(isVowel("A")).toBe(true);
+    expect(isVowel("a")).toBeTruthy();
   });
 
   it("Deberia retornar false para input 'c'", () => {
     expect(isVowel("c")).toBe(false);
+  });
+});
+
+describe("countVowels", () => {
+  it('Deberia retornar { a: 2, e: 3, i: 4, o: 2, u: 8 }', () => {
+    const str = "aaeeeiiiioouuuuuuuu";
+    expect(countVowels(str)).toEqual({ a: 2, e: 3, i: 4, o: 2, u: 8 });
   });
 });
